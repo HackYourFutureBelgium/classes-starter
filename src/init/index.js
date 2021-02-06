be7d1efe19bee4b8f26b93c653ec740399866439
chiok <chiokzechariah@gmail.com>
2021-02-06 14:17:30 +0100

    update all initial files

--- src/init/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// import all listener files so they can attach to the DOM
-
-// does your project have code that executes when the document or window are ready?
-// that code can go here
